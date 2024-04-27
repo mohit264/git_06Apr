175f01a601ec3d5d64d217e8cdb1d5f224b8cfff
Mohit Dharmadhikari <mohit.dharma@gmail.com>
2024-04-27 11:00:45 +0530

    Added User Authentication Functionality

---
+++ user-reg.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void UserLogin(string userName, string pwd)
+{
+	printf("Login successful");
+}
+
+int main(void)
+{
+	printf("User Authentication\n Implemented");
+	return 0;
+}
+
