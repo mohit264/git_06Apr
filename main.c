27c7248cf1acdce7c57b5f1ebd557d8982252e22
Mohit Dharmadhikari <mohit.dharma@gmail.com>
2024-04-27 11:11:59 +0530

    Added User Registration implementation

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+void UserRegistration(string firstName, string lastName, string email)
+{
+	printf("User Registration Successful");
+}
+
+int main (void)
+{
+	printf ("User Registration Implemented");
+	UserRegistration();
+
+	return 0;
+}
+
